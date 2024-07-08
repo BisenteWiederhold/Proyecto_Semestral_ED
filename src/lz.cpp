7f1c9085a56605150b94ffdd910f630a0ff51e80
FMR08 <135428100+FMR08@users.noreply.github.com>
2024-07-07 20:20:58 -0400

    Update lz.cpp

--- src/lz.cpp
+++ src/lz.cpp
@@ -5,11 +5,12 @@
 #include <unordered_map>
 #include <sstream>
 
+//Comprime una cadena de entrada utilizando el algoritmo Lempel-Ziv
 std::vector<std::pair<int, int>> lzCompress(const std::string &input) {
     std::unordered_map<std::string, int> dictionary;
     std::vector<std::pair<int, int>> result;
     std::string buffer;
-    
+    //Este algoritmo construye un diccionario de secuencia encontradas de secuencia de pares
     for (size_t i = 0; i < input.length(); ++i) {
         buffer += input[i];
         if (dictionary.find(buffer) == dictionary.end()) {
@@ -33,12 +34,13 @@ std::vector<std::pair<int, int>> lzCompress(const std::string &input) {
     
     return result;
 }
-
+//Descomprime una secuencia comprimida generada por el algoritmo Lempel-Ziv.
 std::string lzDecompress(const std::vector<std::pair<int, int>> &compressed) {
     std::string result;
     std::string input; 
     int currentIndex = 0; 
-
+    
+    //Este algoritmo reconstruye la cadena original utilizando la secuencia de pares
     for (const auto &pair : compressed) {
         if (pair.second == 0) {
             result += pair.first; 
@@ -55,4 +57,3 @@ std::string lzDecompress(const std::vector<std::pair<int, int>> &compressed) {
     return result;
 
 }
-
