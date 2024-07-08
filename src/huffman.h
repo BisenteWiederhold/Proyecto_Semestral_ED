c594c7b4c7fdf7d7734fff8e2d18d7625ea10860
FMR08 <135428100+FMR08@users.noreply.github.com>
2024-07-07 20:20:12 -0400

    Update huffman.h

--- src/huffman.h
+++ src/huffman.h
@@ -3,16 +3,39 @@
 
 #include <string>
 #include <unordered_map>
+#include <memory>
+#include <queue>
+#include <vector>
+#include <iostream>
 
-struct Node {
-    char symbol;
-    int frequency;
-    Node* left, *right;
+class HuffmanTree {
+public:
+	// Estructura para representar un nodo del árbol de Huffman
+    struct Node {
+        char symbol;
+        int frequency;
+        std::shared_ptr<Node> left;
+        std::shared_ptr<Node> right;
+    };
+    // Functor para comparar nodos por frecuencia (para la cola de prioridad)
+    struct CompareNodes {
+        bool operator()(const std::shared_ptr<Node>& a, const std::shared_ptr<Node>& b) const {
+            return a->frequency > b->frequency;
+        }
+    };
+    //Constructor de la clase HuffmanTree.
+    HuffmanTree(const std::string& text);
+    std::string encode(const std::string& text) const;
+    std::string decode(const std::string& encodedText) const;
+    std::unordered_map<char, std::string> getCodes() const;
+
+private:
+	//genera el arbol y genera los códigos de los caracteres desde la raíz del árbol Huffman.
+    void buildTree(const std::string& text);
+    void buildCodes(std::shared_ptr<Node> node, const std::string& code);
+
+    std::shared_ptr<Node> root;
+    std::unordered_map<char, std::string> codes;
 };
 
-Node* getNode(char symbol, int frequency, Node* left, Node* right);
-void encode(Node* root, std::string str, std::unordered_map<char, std::string>& HuffmanCode);
-void decode(Node* root, int& index, std::string str);
-void createTree(std::string text);
-
-#endif // HUFFMAN_H
+#endif
